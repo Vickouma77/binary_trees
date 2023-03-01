f68e7ad59470f7d07cd415e4822d2d1308ac35ab
vickouma77 <Vickprestone20@gmail.com>
2023-03-01 17:13:44 +0300

    adding missing blank line

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -8,7 +8,7 @@
  * Return: Null if error occurs else pointer to new node
  */
 
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new;
 
