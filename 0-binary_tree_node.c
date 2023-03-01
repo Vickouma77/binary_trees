5625b8c75462a556c97af45f4b2e40693f6fb23f
vickouma77 <Vickprestone20@gmail.com>
2023-03-01 16:29:29 +0300

    including preprocessor directive and function

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,12 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - creates a binary tree node.
+ * @parent: pointer to the parent node
+ * @value: value of new node
+ *
+ * Return: Null if error occurs else pointer to new node
+ */
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+{}
