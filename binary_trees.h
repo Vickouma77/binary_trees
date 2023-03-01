e3d7f8529d24b11d760a7ac4c5d8d50a65d56e95
vickouma77 <Vickprestone20@gmail.com>
2023-03-01 16:15:44 +0300

    updating header file

--- binary_trees.h
+++ binary_trees.h
@@ -20,6 +20,9 @@ struct binary_tree_s
 };
 
 typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s bst_t;
+typedef struct binary_tree_s avl_t;
+typedef struct binary_tree_s heap_t;
 
 /* binary tree prototypes */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
