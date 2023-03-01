a06ce57c061bb6db8954c568991bc9d8f601a7ea
vickouma77 <Vickprestone20@gmail.com>
2023-03-01 16:44:15 +0300

    updating syntax

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -9,4 +9,16 @@
  */
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
-{}
+{
+	binary_tree_t *new;
+	new = malloc(sizeof(binary_tree_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = value;
+	new->parent = parent;
+	new->left = NULL;
+	new->right = NULL;
+
+	return (new);
+}
