995a2874ee19614f641a89ba30b661ff97f9c8c8
vickouma77 <Vickprestone20@gmail.com>
2023-03-02 14:38:25 +0300

    updating syntax

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -3,11 +3,29 @@
 /**
  * binary_tree_insert_left - inserts a node as the left child of another node
  *
- * parent: pointer to the node to insert the left child
- *
- * value: value to store new node
+ * @parent: pointer to the node to insert the left child
+ * @value: value to store new node
  *
  * Return: if success return pointer else return NULL.
  */
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new;
+
+	if (parent == NULL)
+		return (NULL);
+
+	new = binary_tree_node(parent, value);
+	if (new == NULL)
+		return (NULL);
+
+	if (parent->left != NULL)
+	{
+		new->left = parent->left;
+		parent->left->parent = new;
+	}
+	parent->left = new;
+
+	return (new);
+}
