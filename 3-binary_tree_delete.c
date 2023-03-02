04873f2cad1480836015781909f66f7d39d8d493
vickouma77 <Vickprestone20@gmail.com>
2023-03-02 16:35:06 +0300

    deleting entire binary tree

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,11 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - deletes entire binary tree
+ *
+ * @tree: pointer to root node of tree
+ * 
+ * Return: if tree is NULL do nothing
+ */
+
+void binary_tree_delete(binary_tree_t *tree)
