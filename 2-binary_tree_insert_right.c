9df75813d292a6289ec5c47bb901bcc952705425
vickouma77 <Vickprestone20@gmail.com>
2023-03-02 15:17:31 +0300

    inserting right node

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -16,6 +16,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	if (parent == NULL)
 		return (NULL);
 
+	new = binary_tree_node(parent, value);
 	if (new == NULL)
 		return (NULL);
 
