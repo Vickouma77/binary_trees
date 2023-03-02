6ed46177b3ff7191abda7000822cf88e270955c9
vickouma77 <Vickprestone20@gmail.com>
2023-03-02 17:02:12 +0300

    checking root node

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -6,3 +6,10 @@
  * @node: pointer to the node to check
  * Return: 1 if node is root else 0
  */
+
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if (node == NULL || node->parent != NULL)
+		return(0);
+	return (1);
+}
