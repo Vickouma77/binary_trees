99bf1eb464195de9b9bcefc744dba1ec8a1f4c72
vickouma77 <Vickprestone20@gmail.com>
2023-03-02 15:15:17 +0300

    inserting right node

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -16,7 +16,6 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	if (parent == NULL)
 		return (NULL);
 
-	new = malloc(sizeof(binary_tree_t));
 	if (new == NULL)
 		return (NULL);
 
